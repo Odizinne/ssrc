451d5403712c769229019f91a2e67b3d6d9e0fc4
Flora <aethernali.live@gmail.com>
2024-02-05 20:23:40 +0100

    added -c to set hardcoded client resolution

--- steam_stream_resolution_changer.py
+++ steam_stream_resolution_changer.py
@@ -70,7 +70,10 @@ def stop_stream(session_type, default_adapter, default_resolution, wayland_adapt
 def main():
     parser = argparse.ArgumentParser(description="Monitor and adjust streaming settings.")
     parser.add_argument('-a', '--audio', action='store_true', help='Play audio on host')
+    parser.add_argument('-c', '--client-resolution', help='Bypass client resolution detection by using a specified one')
+
     args = parser.parse_args()
+    print(f"{args.client_resolution}")
 
     session_type = os.getenv('XDG_SESSION_TYPE')
     print(f"Session type: {session_type}")
@@ -95,7 +98,11 @@ def main():
 
     with os.popen(command) as tail_output:
         for line in tail_output:
-            if "Maximum capture" in line:
+            if args.client_resolution and "Streaming initialized and listening" in line:
+                streaming_resolution = args.client_resolution 
+                start_stream(streaming_resolution, session_type, default_adapter, filtered_resolutions, wayland_adapter_name, wayland_default_resolution, args)
+
+            if not args.client_resolution and "Maximum capture" in line:
                 match = resolution_pattern.search(line)
                 if match:
                     streaming_resolution = match.group(1)
