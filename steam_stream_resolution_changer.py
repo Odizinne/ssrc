5316ea3db99893f64865d0d8748c2b2eb9173a49
Flora <aethernali.live@gmail.com>
2024-02-05 17:03:42 +0100

    more robust log tracking, fixing lots of issue. Big code refactor for performance and readability

--- steam_stream_resolution_changer.py
+++ steam_stream_resolution_changer.py
@@ -1,13 +1,12 @@
 import os
 import re
 import argparse
-from watchdog.observers import Observer
-from watchdog.events import FileSystemEventHandler
 
-parser = argparse.ArgumentParser(description="Monitor and adjust streaming settings.")
-parser.add_argument('-a', '--audio', action='store_true', help='Play audio on host')
-args = parser.parse_args()
-def get_default_resolution_and_adapter():
+file_path = os.path.expanduser("~/.steam/steam/logs/streaming_log.txt")
+command = f"tail -F {file_path}"
+resolution_pattern = re.compile(r'Maximum capture: (\d+x\d+)')
+
+def get_x11_default_resolution_and_adapter():
     xrandr_output = os.popen("xrandr").read()
     match = re.search(r'(?P<adapter>\w+-\d+) connected primary (?P<resolution>\d+x\d+)', xrandr_output)
 
@@ -16,18 +15,7 @@ def get_default_resolution_and_adapter():
     else:
         raise ValueError("Unable to extract default resolution and adapter from xrandr output")
 
-stream_resolution = None
-session_type = os.getenv('XDG_SESSION_TYPE')
-file_path = os.path.expanduser("~/.steam/steam/logs/streaming_log.txt")
-
-with open(file_path, 'r') as file:
-    initial_lines = file.readlines()
-
-processed_lines = set()
-
-# This function parses gnome-randr output and extract adapter, default res, and every supported resolutions.
-
-def wayland_get_available_resolutions():
+def get_wayland_default_resolution_and_adapter():
     gnome_randr_output = os.popen("gnome-randr").read()
 
     adapter_match = re.search(r'(?:associated physical monitors:\n\s+)([^\s]+)', gnome_randr_output)
@@ -52,82 +40,69 @@ def wayland_get_available_resolutions():
 
     return wayland_adapter_name, wayland_default_resolution, filtered_resolutions
 
-#Display useful informations about your system.
+def start_stream(streaming_resolution, session_type, default_adapter, filtered_resolutions, wayland_adapter_name, wayland_default_resolution, args):
+    print("Stream started")
+    print(f"Requested client resolution: {streaming_resolution}")
 
-print(f"Session type: {session_type}")
+    if session_type == "x11":
+        os.system(f"xrandr --output {default_adapter} --mode {streaming_resolution}")
+    elif session_type == "wayland" and (matching_resolution := next((res for res in filtered_resolutions if streaming_resolution in res), None)):
+        desired_res = matching_resolution
+        print(f"Matching resolution found: {desired_res}")
+        os.system(f"gnome-randr modify --mode {desired_res} {wayland_adapter_name}")
 
-if session_type == "wayland":
-    wayland_adapter_name, wayland_default_resolution, filtered_resolutions = wayland_get_available_resolutions()
-    print("Wayland support is experimental and works only on Gnome with gnome-randr installed.")
-    print(f"Default display Adapter: {wayland_adapter_name}")
-    print(f"Default desktop resolution: {wayland_default_resolution}")
+    if not args.audio:
+        print("Muting host audio")
+        os.system("pactl set-sink-mute @DEFAULT_SINK@ true")
 
-if session_type == "x11":
-    default_adapter, default_resolution = get_default_resolution_and_adapter()
-    print(f"Default display Adapter: {default_adapter}")
-    print(f"Default desktop Resolution: {default_resolution}")
+def stop_stream(session_type, default_adapter, default_resolution, wayland_adapter_name, wayland_default_resolution, args):
+    print("Stream stopped")
 
-if args.audio:
-     print("Playing audio on host: enabled")
-else:
-     print("Playing audio on host: disabled")
+    if session_type == "x11":
+        os.system(f"xrandr --output {default_adapter} --mode {default_resolution}")
+    elif session_type == "wayland":
+        os.system(f"gnome-randr modify --mode {wayland_default_resolution} {wayland_adapter_name}")
 
-# Parse streaming_log.txt to know if streaming is started/stopped, and change resolution accordingly.
-     
-def process_line(line):
-    if "Maximum capture:" in line:
-        match = re.search(r'\d+x\d+', line)
-        if match:
-            stream_resolution = match.group()
-            print(f"Client resolution: {stream_resolution}")
-            if session_type == "x11":
-                os.system(f"xrandr --output {default_adapter} --mode {stream_resolution}")
-            if session_type == "wayland":
-                matching_resolution = next((res for res in filtered_resolutions if stream_resolution in res), None)
-                if matching_resolution:
-                    desired_res = matching_resolution
-                    print(f"Matching resolution found: {desired_res}")
-                    os.system(f"gnome-randr modify --mode {desired_res} {wayland_adapter_name}")
-            if not args.audio:
-                print("Muting host audio")
-                os.system("pactl set-sink-mute @DEFAULT_SINK@ true")
-                
-    elif "terminated" in line and line not in initial_lines and line not in processed_lines:
-        print("Applying default desktop resolution")
-        processed_lines.add(line)
-        if session_type == "x11":
-            os.system(f"xrandr --output {default_adapter} --mode {default_resolution}")
+    if not args.audio:
+        print("Resuming host audio")
+        os.system("pactl set-sink-mute @DEFAULT_SINK@ false")
 
-        if session_type == "wayland":
-            os.system(f"gnome-randr modify --mode {wayland_default_resolution} {wayland_adapter_name}")
+def main():
+    parser = argparse.ArgumentParser(description="Monitor and adjust streaming settings.")
+    parser.add_argument('-a', '--audio', action='store_true', help='Play audio on host')
+    args = parser.parse_args()
 
-        if not args.audio:
-            print("Resuming host audio")
-            os.system("pactl set-sink-mute @DEFAULT_SINK@ false")
+    session_type = os.getenv('XDG_SESSION_TYPE')
+    print(f"Session type: {session_type}")
 
-class StreamStatusMonitor(FileSystemEventHandler):
-    def __init__(self, file_path):
-        self.file_path = file_path
-        self.last_cursor_position = os.stat(self.file_path).st_size
+    default_adapter, default_resolution, wayland_adapter_name, wayland_default_resolution, filtered_resolutions = None, None, None, None, None
 
-    def on_modified(self, event):
-        if event.src_path == self.file_path:
-            with open(self.file_path, 'r') as file:
-                file.seek(self.last_cursor_position)
+    if session_type == "x11":
+        default_adapter, default_resolution = get_x11_default_resolution_and_adapter()
+        print(f"Default display Adapter: {default_adapter}")
+        print(f"Default desktop Resolution: {default_resolution}")
 
-                for line in file:
-                    process_line(line)
+    elif session_type == "wayland":
+        wayland_adapter_name, wayland_default_resolution, filtered_resolutions = get_wayland_default_resolution_and_adapter()
+        print("Wayland support is experimental and works only on Gnome with gnome-randr installed.")
+        print(f"Default display Adapter: {wayland_adapter_name}")
+        print(f"Default desktop resolution: {wayland_default_resolution}")
 
-                self.last_cursor_position = file.tell()
+    if args.audio:
+        print("Playing audio on host: enabled")
+    else:
+        print("Playing audio on host: disabled")
 
-observer = Observer()
-event_handler = StreamStatusMonitor(file_path)
-observer.schedule(event_handler, path=os.path.dirname(file_path), recursive=False)
-observer.start()
+    with os.popen(command) as tail_output:
+        for line in tail_output:
+            if "Maximum capture" in line:
+                match = resolution_pattern.search(line)
+                if match:
+                    streaming_resolution = match.group(1)
+                    start_stream(streaming_resolution, session_type, default_adapter, filtered_resolutions, wayland_adapter_name, wayland_default_resolution, args)
 
-try:
-    observer.join()
-except KeyboardInterrupt:
-    observer.stop()
+            if "connection terminated" in line:
+                stop_stream(session_type, default_adapter, default_resolution, wayland_adapter_name, wayland_default_resolution, args)
 
-observer.join()
+if __name__ == "__main__":
+    main()
