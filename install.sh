2ae8a60e9ca80ca2ced515ad4f96db70f404585b
Odizinne <flora.aubry@proton.me>
2024-02-05 17:38:45 +0100

    added uninstall option

--- install.sh
+++ install.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 usage() {
-    echo "Usage: $0 [--autostart]"
+    echo "Usage: $0 [--autostart] [--uninstall]"
     exit 1
 }
 
 SOURCE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-if [ "$1" == "--autostart" ]; then
-    AUTOSTART=true
+if [ "$1" == "--uninstall" ]; then
+    UNINSTALL=true
 else
-    AUTOSTART=false
+    UNINSTALL=false
 fi
 
 if ! command -v pyinstaller &> /dev/null; then
@@ -18,7 +18,16 @@ if ! command -v pyinstaller &> /dev/null; then
     exit 1
 fi
 
-cd "$SOURCE_DIR"
+if [ "$UNINSTALL" = true ]; then
+    sudo rm -f /usr/local/bin/steam_stream_resolution_changer
+    AUTOSTART_DIR="$HOME/.config/autostart"
+    AUTOSTART_FILE="$AUTOSTART_DIR/steam_stream_resolution_changer.desktop"
+    rm -f "$AUTOSTART_FILE"
+    cd "$SOURCE_DIR"
+    rm -rf build/ dist/ __pycache__/ steam_stream_resolution_changer.spec
+    echo "Uninstallation completed."
+    exit 0
+fi
 
 pyinstaller --onefile steam_stream_resolution_changer.py
 
@@ -28,17 +37,20 @@ rm -rf build/ dist/ __pycache__/ steam_stream_resolution_changer.spec
 
 echo "Installation completed."
 
+if [ "$1" == "--autostart" ]; then
+    AUTOSTART=true
+else
+    AUTOSTART=false
+fi
+
 if [ "$AUTOSTART" = true ]; then
     AUTOSTART_DIR="$HOME/.config/autostart"
     DESKTOP_FILE="$AUTOSTART_DIR/steam_stream_resolution_changer.desktop"
-
     mkdir -p "$AUTOSTART_DIR"
-
     echo "[Desktop Entry]" > "$DESKTOP_FILE"
     echo "Type=Application" >> "$DESKTOP_FILE"
     echo "Name=Steam Stream Resolution Changer" >> "$DESKTOP_FILE"
     echo "Exec=/usr/local/bin/steam_stream_resolution_changer" >> "$DESKTOP_FILE"
-
     echo "Autostart file created: $DESKTOP_FILE"
 fi
 
