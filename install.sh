4813af20523e611e53cad903a7cf06f05e5e4ecf
Odizinne <flora.aubry1994@gmail.com>
2024-02-10 22:01:57 +0100

    replaced desktop file autostart with systemd user service

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ usage() {
 }
 
 SOURCE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+INSTALL_LOCATION="/usr/local/bin"
 
 if [ "$1" == "--uninstall" ]; then
     UNINSTALL=true
@@ -19,23 +20,22 @@ if ! command -v pyinstaller &> /dev/null; then
 fi
 
 if [ "$UNINSTALL" = true ]; then
-    sudo rm -f /usr/local/bin/ssrc
-    AUTOSTART_DIR="$HOME/.config/autostart"
-    AUTOSTART_FILE="$AUTOSTART_DIR/ssrc.desktop"
-    rm -f "$AUTOSTART_FILE"
-    cd "$SOURCE_DIR"
-    rm -rf build/ dist/ __pycache__/ ssrc.spec
+    systemctl --user disable ssrc.service
+    systemctl --user stop ssrc.service
+    sudo rm -f $INSTALL_LOCATION/ssrc
+    USER_SERVICE_DIR="$HOME/.config/systemd/user/"
+    USER_SERVICE="$USER_SERVICE_DIR/ssrc.service"
+    rm -f "$USER_SERVICE"
+    systemctl --user daemon-reload
     echo "Uninstallation completed."
     exit 0
 fi
 
-pyinstaller --onefile ssrc.py
-
-sudo mv dist/ssrc /usr/local/bin/
-
+echo "Building standalone executable..."
+pyinstaller --onefile ssrc.py &>/dev/null
+sudo mv dist/ssrc $INSTALL_LOCATION
+echo "Executable installed in $INSTALL_LOCATION"
 rm -rf build/ dist/ __pycache__/ ssrc.spec
-
-echo "Installation completed."
 
 if [ "$1" == "--autostart" ]; then
     AUTOSTART=true
@@ -44,13 +44,29 @@ else
 fi
 
 if [ "$AUTOSTART" = true ]; then
-    AUTOSTART_DIR="$HOME/.config/autostart"
-    DESKTOP_FILE="$AUTOSTART_DIR/ssrc.desktop"
-    mkdir -p "$AUTOSTART_DIR"
-    echo "[Desktop Entry]" > "$DESKTOP_FILE"
-    echo "Type=Application" >> "$DESKTOP_FILE"
-    echo "Name=Steam Stream Resolution Changer" >> "$DESKTOP_FILE"
-    echo "Exec=/usr/local/bin/ssrc" >> "$DESKTOP_FILE"
-    echo "Autostart file created: $DESKTOP_FILE"
+    USER_SERVICE_DIR="$HOME/.config/systemd/user/"
+    USER_SERVICE="$USER_SERVICE_DIR/ssrc.service"
+    mkdir -p "$USER_SERVICE_DIR"
+        cat <<EOF > "$USER_SERVICE"
+[Unit]
+Description=Workaround for stream remote play match client resolution not working
+StartLimitIntervalSec=500
+StartLimitBurst=5
+
+[Service]
+ExecStart=/usr/local/bin/ssrc
+Restart=on-failure
+RestartSec=5s
+
+[Install]
+WantedBy=graphical-session.target
+EOF
+
+    systemctl --user daemon-reload
+    systemctl --user enable ssrc.service &>/dev/null
+    systemctl --user start ssrc.service
+    echo "Autostart service created and activated"
 fi
 
+echo "Installation completed."
+exit 0
